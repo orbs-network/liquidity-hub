8109ad539b009f14f631ba233a956b17aeb83cca
DanielZlotin <danielzlotin@users.noreply.github.com>
2023-11-16 16:10:42 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { execSync } = require("child_process");
 const path = require("path");
 
 export async function createOrder(chainId, rfq) {
-  const result = execSync(`cd ${path.dirname(__filename)} && CHAIN=${chainId} \
+  const result = execSync(`cd ${path.dirname(__filename)} && \
+  CHAIN=${chainId} \
+  FOUNDRY_BLOCK_TIMESTAMP=${Math.round(Date.now() / 1000)} \
   LH_SWAPPER=${rfq.swapper} \
   LH_INTOKEN=${rfq.inToken} \
   LH_OUTTOKEN=${rfq.outToken} \
