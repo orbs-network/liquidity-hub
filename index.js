0441d75ae4f769602f7b2ab6130e690c184921eb
doron aviguy <doronaviguy@gmail.com>
2023-11-19 14:26:08 +0200

    promisify exec , fix ubuntu encoding issue, add --skip-simulation flag (#1)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const { execSync } = require("child_process");
+const { exec } = require("child_process");
+const promisify = require("util").promisify;
+const execPromise = promisify(exec);
 const path = require("path");
 
 export async function createOrder(chainId, rfq) {
-  const result = execSync(`cd ${path.dirname(__filename)} && \
+  const { stdout, stderr} = await execPromise(`cd ${path.dirname(__filename)} && \
   CHAIN=${chainId} \
   FOUNDRY_BLOCK_TIMESTAMP=${Math.round(Date.now() / 1000)} \
   LH_SWAPPER=${rfq.swapper} \
@@ -10,13 +12,18 @@ export async function createOrder(chainId, rfq) {
   LH_OUTTOKEN=${rfq.outToken} \
   LH_INAMOUNT=${rfq.inAmount} \
   LH_OUTAMOUNT=${rfq.outAmount} \
-  forge script CreateOrder --silent --json`)
-    .toString()
-    .trim();
-  const order = JSON.parse(result).returns;
+  forge script CreateOrder --silent --json --skip-simulation`);
+  if (stderr) {
+    throw new Error(stderr);
+  }
+  const order = JSON.parse(stdout.toString().trim()).returns;
+  let permitData = JSON.parse(order?.permitData.value);
+  if(typeof permitData == "string") {
+      permitData = JSON.parse(permitData);
+  }
   return {
     encoded: order?.encoded.value,
     hash: order?.hash.value,
-    permitData: JSON.parse(order?.permitData.value),
+    permitData: permitData,
   };
 }
