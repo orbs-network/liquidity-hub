ccbbddc597f32014b6d97d1fcb7ef04f8fc24772
doron aviguy <doronaviguy@gmail.com>
2024-01-21 16:43:42 +0200

    rename to repermit

--- script/04_DeployRepermit.s.sol
+++ script/04_DeployRepermit.s.sol
@@ -1,21 +1,21 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.x;
 
 import {RePermit} from "src/RePermit.sol";
 import {Base, Consts} from "script/base/Base.sol";
 
 contract DeployRePermit is Base {
 
-    function run() public returns (address reactor) {
-        reactor = computeCreate2Address(
+    function run() public returns (address rePermit) {
+        rePermit = computeCreate2Address(
             0,
             hashInitCode(type(RePermit).creationCode, address(0))
         );
 
-        if (reactor.code.length == 0) {
+        if (rePermit.code.length == 0) {
             vm.broadcast(deployer);
             require(
-                reactor
+                rePermit
                     == address(new RePermit{salt: 0}())
             );
         } else {
