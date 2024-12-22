bfc82f98ce261d92bf003cef2472e0e0425e2400
DanielZlotin <danielzlotin@users.noreply.github.com>
2024-12-22 11:53:23 +0200

    fee2

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -33,12 +33,13 @@ contract Deploy is BaseScript {
 
         // fee00 = _admin(owner, weth, 0x55669ad6a3db66a4a3bbfe640c9faa64095a75a5228cf52464f4a449257ee6c5);
         // fee01 = _admin(owner, weth, 0xab1462bd378a47c5676f45ed8b1f1de08ddf212e2525b6c82e7c2c11c41590d2);
+        address fee02 = _admin(owner, weth, 0x668fa19c8dfec98130ebcc64b727ecf11105987af78936a05550a1f6679b16cc);
 
         //reactor = _reactor(bytes32(uint256(0)));
         //reactor2 = _reactor(bytes32(uint256(1)));
 
         //executor = _executor(reactor, admin);
-        executor = _executor(0x35db01D1425685789dCc9228d47C7A5C049388d8, 0x000066320a467dE62B1548f46465abBB82662331);
+        //executor = _executor(0x35db01D1425685789dCc9228d47C7A5C049388d8, 0x000066320a467dE62B1548f46465abBB82662331);
 
         //repermit = _repermit();
         //reactorPartial = _partialreactor(repermit);
